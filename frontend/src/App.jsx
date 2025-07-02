ef992d8f17a0e414ac92fe159f1dfdf3020dbe5e
Ren_Izumi <Ren_Izumi@mail.benesse.co.jp>
2025-07-02 18:52:05 +0900

    app-complete 作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import AppBasic from './app-basic/JSX/AppBasic'
+import AppComplete from './app-complete/JSX/AppComplete'
 import AppConfirm from './app-confirm/JSX/AppConfirm'
 import AppFile from './app-file/JSX/AppFile'
 import './App.css'
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/pc/app-basic" element={<AppBasic />} />
                 <Route path="/pc/app-file" element={<AppFile />} />
                 <Route path="/pc/app-confirm" element={<AppConfirm />} />
+                <Route path="/pc/app-complete" element={<AppComplete />} />
                 {/* デフォルトルート - /pc/app-basicにリダイレクト */}
                 <Route path="/" element={<AppBasic />} />
             </Routes>
---
+++ frontend/src/app-complete/JSX/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-left">
+                <div className="app-icon">P</div>
+                <span className="app-title">Power Apps | 申請アプリA12</span>
+            </div>
+            <div className="header-right">
+                <span className="user-info">共有</span>
+            </div>
+        </div>
+    );
+};
+
+export default Header; 
